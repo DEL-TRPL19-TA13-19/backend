265688f937cb1750e50122b2740faa174df7d3ce
nathan-nando <nathan.nandoo@gmail.com>
2023-05-08 09:39:01 +0700

    update critera & convert alternative to point

--- internal/dto/ahp/request.go
+++ internal/dto/ahp/request.go
@@ -1,5 +1,11 @@
 package dto
 
+import "ta13-svc/internal/entity"
+
 type AHPByCollectionIDRequest struct {
 	CollectionID string `json:"collection_id" param:"collection_id" validate:"required"`
+}
+
+type CriteriaAlternativeUpdateRequest struct {
+	Pairwise entity.Matrix `json:"pairwise"`
 }
